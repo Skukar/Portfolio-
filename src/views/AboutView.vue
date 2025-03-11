5cb45ce271120bcacbfc4276de3056d2755417e3
Skukar <101894352+Skukar@users.noreply.github.com>
2025-03-12 01:38:28 +0700

    Add files via upload

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -128,18 +128,21 @@ export default {
         </div>
         <div class="md:w-7/12">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
-            &nbsp; &nbsp; &nbsp; Hi everyone! My name is Bagas Rakha Putra. I'm a web developer from Bogor, West Java. I
-            have 1 year of experience in back-end web development.
-            I really enjoy what I do right now, in my opinion, creating programs is not just a job, but also an art that
-            has
-            aesthetic value
-          </p>
-          <p class="mb-3 fadein-left fadeins-2">
-            &nbsp; &nbsp; &nbsp; My job is to build your website to be functional and user-friendly yet still attractive.
-            In addition, I provide a personal touch to your product and ensure that the website catches attention and is
-            easy to use.
-            My goal is to convey your message and identity in the most creative way. If you are interested in hiring me,
-            please contact the listed contact.
+            &nbsp; &nbsp; &nbsp; halo nama gua
+              Adit Zandika gua tinggal di bekasi
+              ya mungkin ini pertama kali gua
+              buat portfololio dan yap 
+              ini bukan pure punya gua 
+              ini gua dapet dari bang bagas rakha
+              makasih buat bang bagas udah ngasih
+              soure codenya dan yap ini gua mau 
+              belajar ngoding namun gua sedang 
+              mencari referensi dan sekailian 
+              belajar otodidak ya walau ga bisa
+              terlalu banyak, dan sekali lagi
+              buat bang bagas makasih scnya
+              mungkin gua bakalan
+              belajar bahasa vue vetur 
           </p>
         </div>
 
