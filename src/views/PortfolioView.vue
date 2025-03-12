9ddc9cd00bd0a797252dd437e81cc267854bf664
Adit Zandika <101894352+Skukar@users.noreply.github.com>
2025-03-12 21:39:32 +0700

    Add files via upload

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -11,34 +11,7 @@ export default {
           status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
           tech: 'VueJS 3, Tailwind',
           github: 'https://github.com/Skukar/Portfolio-',
-          demo: 'https://gelo1.netlify.app/'
-        },
-        {
-          id: 2,
-          name: 'SISTEKDES',
-          imageUrl: 'sistekdes',
-          status: 'Sistem Informasi dan Teknologi Desa, a web application to assist village government work in administration, population data management, community services, etc.',
-          tech: 'Laravel 9, Bootstrap',
-          github: 'null',
-          demo: 'null'
-        },
-        {
-          id: 3,
-          name: 'Bumdesma',
-          imageUrl: 'bumdesma',
-          status: 'a web application used by village-owned enterprises at the sub-district level to make it easier for them in terms of accounting that is equipped with a microfinance balance.',
-          tech: 'Laravel 8, Bootstrap',
-          github: 'null',
-          demo: 'null'
-        },
-        {
-          id: 4,
-          name: 'SP2SE',
-          imageUrl: 'sp2se',
-          status: 'Sistem Percepatan Pelayanan Surat Elektronik, A web application to manage letters in the village.',
-          tech: 'Codeigniter 3, Bootstrap',
-          github: 'null',
-          demo: 'null'
+          demo: 'hsoon'
         }
       ]
     };
